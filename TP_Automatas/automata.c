917edd0ac31f5650165a328de16f9f1fd6bdd7df
AgostinaEscobar <aescobar@frba.utn.edu.ar>
2025-09-20 00:11:28 -0300

    Bases de un Automata (lo que vimos en clase nomas)

--- TP_Automatas/automata.c
+++ TP_Automatas/automata.c
@@ -1,1 +1,64 @@
+int columna (char c) //{}
+{
+  if( perteneceAlGrupo(c, int grupo)) // podemos hacer una funcion que nos retorne el grupo directamente
+  {
+    return i;    
+  }
+  return 0;
+}
 
+int esPalabra(char* cadena)
+{
+  static int tt^[fila][columna]= { {},
+                                   {},
+                                   {},
+                                   {},
+                                   {},
+                                   {} };
+  int e=0; // guarda estado
+  int i=0; // puntero de cadena
+  int c = cadena[i]; // guarda char de pos i en la cadena
+  while (c!='\0')
+  {
+    e= tt[e][columna(c)];
+    i++;
+    c= cadena[i];
+  }
+  if (esEstadoDeAceptacion(e)) // usando paradigma funcional
+    return 1;     // si e es estado de aceptacion, es valida
+  return 0;
+}
+
+int verifica(char* cadena) // verifica que la cadena contenga solo caracteres de alguna de las columnas
+{
+  int i=0;
+  int c = cadena[i];
+  while (c!='\0')
+  {
+    if (pertenceAlAlfabeto(c))
+      return 0;
+    i++;
+    c = cadena[i];
+  }
+  return 1;
+}
+
+int main(int longitud, char *cadena[])
+{
+  
+  if (verifica(cadena))
+  {
+    if (esPalabra(cadena))
+    {
+      printf ("es palabra del lenguaje");      
+    }
+    else
+    {
+      printf ("no es palabra del lenguaje");      
+    }
+  }
+  else
+  {
+    printf ("posee caracteres invalidos"); 
+  }
+}
