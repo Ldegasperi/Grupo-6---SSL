f29df3effe6d7b481273fda3e53ff34fcf6bd9b8
Valentina Aguilera <vaguilera@frba.utn.edu.ar>
2025-09-21 20:51:28 -0300

    Add files via upload

---
+++ TP_Automatas/Intento de punto 3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+int esOperador(char c){
+     return (c == '+' || c == '-' || c == '*' || c == '/');
+}
+
+
+int esValido(char c) {
+    return (isdigit(c) || esOperador(c));
+}
+
+int validarCdena (char *expresion){
+    int i = strlen (expresion);
+    if (i==0) return 0;
+    if(esOperador(expresion[0]|| esOperador(expresion[i-1])))return 0;
+
+    for(int j=0; j<i;i++){
+        if(!esValido(expresion[i])) return 0;
+    }
+    if (esOperador(expresion[j]) && esOperador(expresion[j+1]))return 0;
+
+return 1;
+}
